4fbf6b72fb32add36bc6b2a1480556865e0193d2
Othman Ahmed <othmanahmed258@gmail.com>
2026-04-27 17:32:40 +0300

    create: the secondary navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 
+import { IoIosSearch } from "react-icons/io";
+
 const Navbar = () => {
   return (
-    <nav className="fixed w-full top-0 left-0 z-10 mt-[2rem]">
-      <section className="max-w-[1800px] mx-auto flex items-center justify-between">
+    <nav className="fixed w-full top-0 left-0 z-10 mt-[1rem]">
+      <section className="max-w-[1850px] mx-auto flex items-center justify-between">
         <Link
           href="/"
           className="text-light text-[1.5rem] font-[500] tracking-[.08em] compress-text-height"
@@ -17,7 +19,36 @@ const Navbar = () => {
         </button>
       </section>
 
-      <section className="absolute"></section>
+      <section className="absolute top-[1rem] right-[10rem] text-light text-[1rem] font-[500] flex items-start gap-[17rem]">
+        <section className="flex flex-col">
+          <Link href="/" className="hover:underline">
+            Projects
+          </Link>
+          <Link href="/" className="hover:underline">
+            Explore
+          </Link>
+          <Link href="/" className="hover:underline">
+            Disciplines
+          </Link>
+        </section>
+
+        <section className="flex flex-col">
+          <Link href="/" className="hover:underline">
+            Sustainability
+          </Link>
+          <Link href="/" className="hover:underline">
+            Digital Future
+          </Link>
+          <Link href="/" className="hover:underline">
+            Careers
+          </Link>
+        </section>
+
+        <section className="flex items-center gap-[.5rem]">
+          <span className="hover:underline">Search</span>{" "}
+          <IoIosSearch className="w-[1rem]" />
+        </section>
+      </section>
     </nav>
   );
 };
