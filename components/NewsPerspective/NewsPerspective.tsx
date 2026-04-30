7abe96440f4a5d495acc12b14e1909e0c2ad057a
Othman Ahmed <othmanahmed258@gmail.com>
2026-04-30 12:31:43 +0300

    create: news and perspective slider functionality

--- components/NewsPerspective/NewsPerspective.tsx
+++ components/NewsPerspective/NewsPerspective.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 import { RiArrowRightLongLine } from "react-icons/ri";
 import { RiCornerDownRightLine } from "react-icons/ri";
 import { RiArrowLeftLongLine } from "react-icons/ri";
 import { newsPerspective } from "@/data/news";
-import Image from "next/image";
 
 const NewsPerspective = () => {
+  const [slider, setSlider] = useState(0);
+  console.log("slider value: ", slider);
+  console.log("news length", newsPerspective.length - 1);
+
   return (
     <section className="bg-light py-[5rem] w-full overflow-hidden">
       <section className="max-w-[1600px] mx-auto">
@@ -14,10 +21,31 @@ const NewsPerspective = () => {
           <h2 className="font-[400] text-[1.75rem]">News & Perspectives</h2>
 
           <section className="flex items-center gap-[.5rem]">
-            <button className="border border-dark p-[.5rem] rounded-[.2rem]">
+            <button
+              className="border border-dark p-[.5rem] rounded-[.2rem]"
+              onClick={() => {
+                if (slider <= 1) {
+                  setSlider(0);
+                  console.log("left slider: ", slider);
+                } else {
+                  setSlider(slider - 2);
+                  console.log("left slider: ", slider);
+                }
+              }}
+            >
               <RiArrowLeftLongLine />
             </button>
-            <button className="border border-dark p-[.5rem] rounded-[.2rem]">
+
+            <button
+              className="border border-dark p-[.5rem] rounded-[.2rem]"
+              onClick={() => {
+                if (slider + 2 >= newsPerspective.length - 1) {
+                  setSlider(newsPerspective.length - 2);
+                } else {
+                  setSlider(slider + 2);
+                }
+              }}
+            >
               <RiArrowRightLongLine />
             </button>
             <Link
@@ -35,23 +63,26 @@ const NewsPerspective = () => {
             <Link
               key={item.id}
               href="/"
-              className="flex-shrink-0 w-[49.5%] mr-[1%] relative"
+              style={{ transform: `translateX(${slider * -102}%)` }}
+              className="flex-shrink-0 w-[49.5%] mr-[1%] relative group transition-transform duration-500 ease-out"
             >
-              <span className="absolute top-[1rem] left-[1rem] px-[.5rem] py-[.2rem] bg-light rounded-[.2rem] text-[.8rem] font-[500]">
+              <span className="absolute top-[1rem] left-[1rem] px-[.5rem] py-[.2rem] bg-light rounded-[.2rem] text-[.8rem] font-[500] z-[5]">
                 {item.type}
               </span>
 
-              <Image
-                src={item.thumbNail}
-                alt={item.title}
-                width={500}
-                height={500}
-                className="w-full h-full rounded-[.2rem]"
-              />
+              <section className="overflow-hidden">
+                <Image
+                  src={item.thumbNail}
+                  alt={item.title}
+                  width={500}
+                  height={500}
+                  className="w-full h-full rounded-[.2rem] group-hover:scale-105 transition-all ease-out duration-700"
+                />
+              </section>
 
               <p className="mt-[.5rem] text-[.8rem] font-[400]">{item.date}</p>
 
-              <h3 className="mt-[.5rem] text-[1.25rem] font-[500] hover:underline tracking-tighter">
+              <h3 className="mt-[.5rem] text-[1.25rem] font-[500] group-hover:underline tracking-tighter">
                 {item.title}
               </h3>
             </Link>
