f8507c4e96288995e74c660959d401c1abf27859
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-01 00:37:40 +0300

    fix: CTA button position in sphere section

--- components/Sphere/Sphere.tsx
+++ components/Sphere/Sphere.tsx
@@ -16,15 +16,17 @@ const Sphere = () => {
 
       <section className="absolute inset-0 max-w-[1600px] mx-auto pt-[20rem] pb-[10rem] min-h-[200vh]">
         <section className="sticky top-[20rem] flex items-start justify-between text-light w-full">
-          <p className="text-[2.25rem]">Sphere</p>
+          <p className="text-[2.25rem] flex-1">Sphere</p>
 
-          <Link
-            href="/"
-            className="flex items-center gap-[1rem] px-[.5rem] py-[.45rem] text-[.8rem] font-[500] bg-light text-dark hover:bg-theme hover:text-white rounded-[.2rem]"
-          >
-            <RiCornerDownRightLine className="text-[1rem]" />
-            View Project
-          </Link>
+          <section className="flex-1 flex items-center justify-center">
+            <Link
+              href="/"
+              className="w-fit flex items-center gap-[1rem] px-[.5rem] py-[.45rem] text-[.8rem] font-[500] bg-light text-dark hover:bg-theme hover:text-white rounded-[.2rem]"
+            >
+              <RiCornerDownRightLine className="text-[1rem]" />
+              View Project
+            </Link>
+          </section>
         </section>
       </section>
     </section>
