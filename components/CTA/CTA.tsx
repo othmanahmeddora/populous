8e60cb2fdefe2709e4eddb1362095b2fb28e8721
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-01 15:31:59 +0300

    create: CTA structure and styles

--- components/CTA/CTA.tsx
+++ components/CTA/CTA.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import Link from "next/link";
+
+import { RiCornerDownRightLine } from "react-icons/ri";
 
 const CTA = () => {
   return (
     <section className="bg-light py-[10rem] px-[1rem]">
-      <section className="text-center"></section>
+      <section className="flex flex-col items-center justify-between gap-[5rem]">
+        <section className="text-center text-[5rem] tracking-tighter leading-[5.5rem]">
+          <p>Have a question?</p>
+
+          <p>Want to work with us?</p>
+        </section>
+
+        <Link
+          href="/"
+          className="w-fit flex items-end gap-[.5rem] px-[.5rem] py-[.45rem] text-[.8rem] font-[500] bg-dark text-light hover:bg-theme hover:text-white rounded-[.3rem]"
+        >
+          <RiCornerDownRightLine className="text-[1rem]" />
+          Contact
+        </Link>
+      </section>
     </section>
   );
 };
