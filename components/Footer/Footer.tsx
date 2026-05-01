9092d0ef586d19e7ecfa8364cc8ea540b4d5782c
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-01 17:35:07 +0300

    create: footer folder, file, and data

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-theme h-screen">
+      <section className="max-w-[1600px] flex flex-col items-start justify-between">
+        <h2>Populous</h2>
+
+        <section></section>
+
+        <section></section>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ data/footerLinks.ts
@@ -0,0 +1,96 @@
+export const footerLinks = [
+  {
+    id: 1,
+    title: "What",
+    links: [
+      {
+        id: 1,
+        title: "Explore",
+        link: "/",
+      },
+      {
+        id: 2,
+        title: "Projects",
+        link: "/",
+      },
+      {
+        id: 3,
+        title: "Disciplines",
+        link: "/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Who",
+    links: [
+      {
+        id: 1,
+        title: "About",
+        link: "/",
+      },
+      {
+        id: 2,
+        title: "Team",
+        link: "/",
+      },
+      {
+        id: 3,
+        title: "Careers",
+        link: "/",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Impact",
+    links: [
+      {
+        id: 1,
+        title: "Sustainability",
+        link: "/",
+      },
+      {
+        id: 2,
+        title: "Digital Future",
+        link: "/",
+      },
+      {
+        id: 3,
+        title: "News",
+        link: "/",
+      },
+      {
+        id: 4,
+        title: "Contact",
+        link: "/",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Social",
+    links: [
+      {
+        id: 1,
+        title: "LinkedIn",
+        link: "/",
+      },
+      {
+        id: 2,
+        title: "Facebook",
+        link: "/",
+      },
+      {
+        id: 3,
+        title: "Instagram",
+        link: "/",
+      },
+      {
+        id: 4,
+        title: "X",
+        link: "/",
+      },
+    ],
+  },
+];
