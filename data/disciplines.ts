db5218b0375a0fcf70eddaa6a85dc274db3ec38d
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-01 13:52:56 +0300

    create: disciplines data

---
+++ data/disciplines.ts
@@ -0,0 +1,47 @@
+export const disciplines = [
+  {
+    id: 1,
+    title: "Architecture",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Audiovisual Consultancy",
+    link: "/",
+  },
+  {
+    id: 3,
+    title: "Brand Activation",
+    link: "/",
+  },
+  {
+    id: 4,
+    title: "Event",
+    link: "/",
+  },
+  {
+    id: 5,
+    title: "Food & Beverage Strategy",
+    link: "/",
+  },
+  {
+    id: 6,
+    title: "Interior Design",
+    link: "/",
+  },
+  {
+    id: 7,
+    title: "Landscape Architecture",
+    link: "/",
+  },
+  {
+    id: 8,
+    title: "Urban Design",
+    link: "/",
+  },
+  {
+    id: 9,
+    title: "Wayfinding",
+    link: "/",
+  },
+];
