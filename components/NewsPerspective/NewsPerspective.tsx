6e5c3361e9804b99d5d3649495a2a3580046fbaf
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-02 16:15:35 +0300

    create: responsivness for news-perepective section

--- components/NewsPerspective/NewsPerspective.tsx
+++ components/NewsPerspective/NewsPerspective.tsx
@@ -13,37 +13,45 @@ const NewsPerspective = () => {
   const [slider, setSlider] = useState(0);
 
   return (
-    <section className="bg-light py-[10rem] w-full overflow-hidden px-[1rem]">
+    <section className="bg-light xl:py-[10rem] py-[5rem] w-full overflow-hidden px-[1rem]">
       <section className="max-w-[1600px] mx-auto">
-        <section className="flex items-center justify-between mb-[1rem]">
-          <h2 className="font-[400] text-[1.75rem]">News & Perspectives</h2>
+        <section className="flex xl:flex-row flex-col xl:items-center items-start justify-between xl:gap-0 gap-[1rem] mb-[1rem]">
+          <h2 className="flex-1 font-[400] text-[1.75rem]">
+            News & Perspectives
+          </h2>
 
-          <section className="flex items-center gap-[.5rem]">
-            <button
-              className={`border border-dark p-[.5rem] rounded-[.2rem] ${slider === 0 ? "text-light-gray border-light-gray" : "hover:bg-theme hover:text-light hover:border-theme"}`}
-              onClick={() => {
-                if (slider <= 1) {
-                  setSlider(0);
-                } else {
-                  setSlider(slider - 2);
-                }
-              }}
-            >
-              <RiArrowLeftLongLine />
-            </button>
+          <section className="flex-1 flex items-center xl:justify-end justify-between w-full xl:ga gap-[.5rem]">
+            <section className="flex items-center gap-[1rem]">
+              <button
+                className={`border border-dark p-[.5rem] rounded-[.2rem] ${slider === 0 ? "text-light-gray border-light-gray" : "hover:bg-theme hover:text-light hover:border-theme"}`}
+                onClick={() => {
+                  const step = window.innerWidth < 768 ? 1 : 2;
 
-            <button
-              className={`border border-dark p-[.5rem] rounded-[.2rem] ${slider === newsPerspective.length - 2 ? "text-light-gray border-light-gray" : "hover:bg-theme hover:text-light hover:border-theme"}`}
-              onClick={() => {
-                if (slider + 2 >= newsPerspective.length - 1) {
-                  setSlider(newsPerspective.length - 2);
-                } else {
-                  setSlider(slider + 2);
-                }
-              }}
-            >
-              <RiArrowRightLongLine />
-            </button>
+                  if (slider <= 1) {
+                    setSlider(0);
+                  } else {
+                    setSlider(slider - step);
+                  }
+                }}
+              >
+                <RiArrowLeftLongLine />
+              </button>
+
+              <button
+                className={`border border-dark p-[.5rem] rounded-[.2rem] ${slider === newsPerspective.length - 2 ? "text-light-gray border-light-gray" : "hover:bg-theme hover:text-light hover:border-theme"}`}
+                onClick={() => {
+                  const step = window.innerWidth < 768 ? 1 : 2;
+
+                  if (slider + 2 >= newsPerspective.length - 1) {
+                    setSlider(newsPerspective.length - 2);
+                  } else {
+                    setSlider(slider + step);
+                  }
+                }}
+              >
+                <RiArrowRightLongLine />
+              </button>
+            </section>
 
             <Link
               href="/"
@@ -61,7 +69,7 @@ const NewsPerspective = () => {
               key={item.id}
               href="/"
               style={{ transform: `translateX(${slider * -102}%)` }}
-              className="flex-shrink-0 w-[49.5%] mr-[1%] relative group transition-transform duration-500 ease-out"
+              className="flex-shrink-0 xl:w-[49.5%] w-[100%] xl:mr-[1%] mr-[2%] relative group transition-transform duration-500 ease-out"
             >
               <span className="absolute top-[1rem] left-[1rem] px-[.5rem] py-[.2rem] bg-light rounded-[.2rem] text-[.8rem] font-[500] z-[5]">
                 {item.type}
