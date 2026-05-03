95ed473cfb8bb2d5cc8c88f09c708a66ff32f784
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-03 15:56:28 +0300

    create: sub-nav links data

---
+++ data/sub-navLinks.ts
@@ -0,0 +1,42 @@
+export const subNavLinks = [
+  {
+    id: 1,
+    links: [
+      {
+        id: 1,
+        title: "Projects",
+        link: "/",
+      },
+      {
+        id: 2,
+        title: "Explore",
+        link: "/",
+      },
+      {
+        id: 1,
+        title: "Discipline",
+        link: "/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    links: [
+      {
+        id: 1,
+        title: "Sustainability",
+        link: "/",
+      },
+      {
+        id: 2,
+        title: "Digital Future",
+        link: "/",
+      },
+      {
+        id: 1,
+        title: "Careers",
+        link: "/",
+      },
+    ],
+  },
+];
