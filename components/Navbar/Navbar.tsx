c0d39724fa2ea1f9b697cd6b981e955929bf0336
Othman Ahmed <othmanahmed258@gmail.com>
2026-05-12 15:52:07 +0300

    fix: z indexes in navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -102,9 +102,9 @@ const Navbar = ({ navColor = "light" }) => {
 
   return (
     <nav className="relative">
-      <section className="fixed top-[1rem] left-0 w-full z-15 mt-[1rem] px-[1rem] bg-transparent">
+      <section className="fixed top-[1rem] left-0 w-full mt-[1rem] z-30 px-[1rem] bg-transparent">
         {/* MAIN NAVBAR */}
-        <section>
+        <section className="z-10">
           <section className="max-w-[1850px] mx-auto flex items-center justify-between">
             <section className="h-[2rem] overflow-hidden">
               <section
@@ -143,108 +143,108 @@ const Navbar = ({ navColor = "light" }) => {
             </button>
           </section>
         </section>
+      </section>
 
-        {/* OVERLAY FOR MENU */}
-        <section
-          ref={menuOverlay}
-          className="fixed top-0 left-0 h-screen w-screen bg-black/50 opacity-0 z-10 pointer-events-none"
-        ></section>
+      {/* OVERLAY FOR MENU */}
+      <section
+        ref={menuOverlay}
+        className="fixed top-0 left-0 h-screen w-screen bg-black/50 opacity-0 z-15 pointer-events-none"
+      ></section>
 
-        {/* MENU */}
-        <section
-          ref={navMenu}
-          className="fixed 3xl:top-[1.5rem] top-[0] 3xl:right-[1.5rem] right-0 [clip-path:polygon(0%_0%,100%_0%,100%_0%,0%_0%)] z-30 bg-light 2xl:rounded-[.5rem] rounded-0 3xl:h-[95%] h-[100%] 4xl:w-[50%] 3xl:w-[70%] w-[100%]"
-        >
-          <TfiClose
-            onClick={() => setIsActive(false)}
-            className="absolute top-[1rem] right-[1rem] text-[1.5rem] cursor-pointer"
-          />
+      {/* MENU */}
+      <section
+        ref={navMenu}
+        className="fixed 3xl:top-[1.5rem] top-[0] 3xl:right-[1.5rem] right-0 [clip-path:polygon(0%_0%,100%_0%,100%_0%,0%_0%)] z-32 bg-light 2xl:rounded-[.5rem] rounded-0 3xl:h-[95%] h-[100%] 4xl:w-[50%] 3xl:w-[70%] w-[100%]"
+      >
+        <TfiClose
+          onClick={() => setIsActive(false)}
+          className="absolute top-[1rem] right-[1rem] text-[1.5rem] cursor-pointer"
+        />
 
-          <section className="flex flex-col items-start justify-between h-full p-[1rem]">
-            <section className="flex xl:flex-row flex-col-reverse items-start justify-between w-full h-full xl:mt-0 mt-[4rem]">
-              <nav className="xl:flex-1 flex flex-col gap-0 text-[3rem] leading-[3rem] h-full">
-                <div className="overflow-hidden h-[3.5rem]">
-                  <Link
-                    ref={(el) => {
-                      bigNavLinks.current[0] = el;
-                    }}
-                    href="/"
-                    className="hover:underline w-fit block"
-                  >
-                    Explore
-                  </Link>
-                </div>
+        <section className="flex flex-col items-start justify-between h-full p-[1rem]">
+          <section className="flex xl:flex-row flex-col-reverse items-start justify-between w-full h-full xl:mt-0 mt-[4rem]">
+            <nav className="xl:flex-1 flex flex-col gap-0 text-[3rem] leading-[3rem] h-full">
+              <div className="overflow-hidden h-[3.5rem]">
+                <Link
+                  ref={(el) => {
+                    bigNavLinks.current[0] = el;
+                  }}
+                  href="/"
+                  className="hover:underline w-fit block"
+                >
+                  Explore
+                </Link>
+              </div>
 
-                <div className="overflow-hidden h-[3.5rem]">
-                  <Link
-                    ref={(el) => {
-                      bigNavLinks.current[1] = el;
-                    }}
-                    href="/projects"
-                    className="hover:underline w-fit block"
-                  >
-                    Projects
-                  </Link>
-                </div>
+              <div className="overflow-hidden h-[3.5rem]">
+                <Link
+                  ref={(el) => {
+                    bigNavLinks.current[1] = el;
+                  }}
+                  href="/projects"
+                  className="hover:underline w-fit block"
+                >
+                  Projects
+                </Link>
+              </div>
 
-                <div className="overflow-hidden h-[3.5rem]">
-                  <Link
-                    ref={(el) => {
-                      bigNavLinks.current[2] = el;
-                    }}
-                    href="/"
-                    className="hover:underline w-fit block"
-                  >
-                    Disciplines
-                  </Link>
-                </div>
+              <div className="overflow-hidden h-[3.5rem]">
+                <Link
+                  ref={(el) => {
+                    bigNavLinks.current[2] = el;
+                  }}
+                  href="/"
+                  className="hover:underline w-fit block"
+                >
+                  Disciplines
+                </Link>
+              </div>
+            </nav>
+
+            <button className="xl:flex-1 flex items-center gap-[.5rem]">
+              <span className="hover:underline font-[400]">Search</span>{" "}
+              <IoIosSearch className="w-[1rem]" />
+            </button>
+          </section>
+
+          <section className="flex flex-col gap-[2rem] w-full">
+            <section className="flex items-start justify-between w-full">
+              <section className="flex-1 font-[500] text-light-gray">
+                Who we are
+              </section>
+
+              <nav className="flex-1 flex flex-col font-[500] leading-[1.4rem]">
+                <Link className="hover:underline" href="/">
+                  About
+                </Link>
+                <Link className="hover:underline" href="/">
+                  Team
+                </Link>
+                <Link className="hover:underline" href="/">
+                  Careers
+                </Link>
               </nav>
-
-              <button className="xl:flex-1 flex items-center gap-[.5rem]">
-                <span className="hover:underline font-[400]">Search</span>{" "}
-                <IoIosSearch className="w-[1rem]" />
-              </button>
             </section>
 
-            <section className="flex flex-col gap-[2rem] w-full">
-              <section className="flex items-start justify-between w-full">
-                <section className="flex-1 font-[500] text-light-gray">
-                  Who we are
-                </section>
-
-                <nav className="flex-1 flex flex-col font-[500] leading-[1.4rem]">
-                  <Link className="hover:underline" href="/">
-                    About
-                  </Link>
-                  <Link className="hover:underline" href="/">
-                    Team
-                  </Link>
-                  <Link className="hover:underline" href="/">
-                    Careers
-                  </Link>
-                </nav>
+            <section className="flex items-start justify-between w-full">
+              <section className="flex-1 font-[500] text-light-gray">
+                Impact
               </section>
 
-              <section className="flex items-start justify-between w-full">
-                <section className="flex-1 font-[500] text-light-gray">
-                  Impact
-                </section>
-
-                <nav className="flex-1 flex flex-col font-[500] leading-[1.4rem]">
-                  <Link className="hover:underline" href="/">
-                    Sustainability
-                  </Link>
-                  <Link className="hover:underline" href="/">
-                    Digital Future
-                  </Link>
-                  <Link className="hover:underline" href="/">
-                    News
-                  </Link>
-                  <Link className="hover:underline" href="/">
-                    Contact
-                  </Link>
-                </nav>
-              </section>
+              <nav className="flex-1 flex flex-col font-[500] leading-[1.4rem]">
+                <Link className="hover:underline" href="/">
+                  Sustainability
+                </Link>
+                <Link className="hover:underline" href="/">
+                  Digital Future
+                </Link>
+                <Link className="hover:underline" href="/">
+                  News
+                </Link>
+                <Link className="hover:underline" href="/">
+                  Contact
+                </Link>
+              </nav>
             </section>
           </section>
         </section>
@@ -252,7 +252,7 @@ const Navbar = ({ navColor = "light" }) => {
 
       {/* SUB NAVBAR */}
       <section
-        className={`absolute top-[3rem] right-[10rem] text-${navColor} text-[1rem] font-[500] 3xl:flex items-start gap-[17rem] z-15 4xl:opacity-100 opacity-0 4xl:pointer-events-auto pointer-events-none`}
+        className={`absolute top-[3rem] right-[10rem] text-${navColor} text-[1rem] font-[500] 3xl:flex items-start gap-[17rem] z-31 4xl:opacity-100 opacity-0 4xl:pointer-events-auto pointer-events-none`}
       >
         {subNavLinks.map((subLink) => (
           <section key={subLink.id} className="flex flex-col">
